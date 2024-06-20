7cfee81084ee355edc7e23b27b974eec39dd8327
riva773 <teslaicaer@gmail.com>
2024-06-20 17:09:48 +0900

    fix:フォロー、フォロー解除の統合テストが通過するように修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_05_081707) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_20_071405) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "user_id", null: false
+    t.string "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "follows", force: :cascade do |t|
     t.integer "follower_id"
@@ -20,6 +58,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_05_081707) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["follower_id", "followed_id"], name: "index_follows_on_follower_id_and_followed_id", unique: true
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "post_tags", force: :cascade do |t|
@@ -47,6 +94,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_05_081707) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_tags", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "tag_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tag_id"], name: "index_user_tags_on_tag_id"
+    t.index ["user_id"], name: "index_user_tags_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
@@ -61,7 +117,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_05_081707) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "post_tags", "posts"
   add_foreign_key "post_tags", "tags"
   add_foreign_key "posts", "users"
+  add_foreign_key "user_tags", "tags"
+  add_foreign_key "user_tags", "users"
 end
--- spec/system/follows_spec.rb
+++ spec/system/follows_spec.rb
@@ -13,8 +13,16 @@ RSpec.describe "Follows", type: :system do
       # ユーザー２の詳細ページへ移動する
       visit user_path(@user2)
       #ユーザー２をフォローする
-
+      click_button('フォロー')
+      # ユーザー１がユーザー２をフォローしているか確認
+      expect(page).to have_selector '.unfollow_btn', text: 'フォロー解除'
+      expect(@user1.followings.count).to eq(1)
+      expect(@user2.followeds.count).to eq(1)
       #ユーザー２をフォロー解除する
+      click_button('フォロー解除')
+      expect(page).to have_selector '.follow_btn', text: 'フォロー'
+      expect(@user1.followings.count).to eq(0)
+      expect(@user2.followeds.count).to eq(0)
     end
   end
 end
